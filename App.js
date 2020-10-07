bfee059e066ae56771bcbdf2d326eec9f669083b
yleia-s <yleiaspotlist@gmail.com>
2020-10-06 18:57:54 -0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  stretch: {
+    width: 500,
+    height: 50,
+    resizeMode: "stretch",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
   },
 });
+
+const Page = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={{ width: 150, height: 150 }}
+        source={require("./assets/splash.png")}
+      />
+      <Image
+        style={{ height: 50 }}
+        source={require("./assets/selectIndustry.png")}
+      />
+      <Image style={styles.stretch} source={require("./assets/Barber.png")} />
+      <Image
+        style={styles.stretch}
+        source={require("./assets/Beautician.png")}
+      />
+      <Image style={styles.stretch} source={require("./assets/Makeup.png")} />
+      <Image style={styles.stretch} source={require("./assets/Nails.png")} />
+      <Image
+        style={styles.stretch}
+        source={require("./assets/Esthetician.png")}
+      />
+      <Image style={styles.stretch} source={require("./assets/Message.png")} />
+      <Image
+        style={{ flexDirection: "column", justifyContent: "center" }}
+        source={require("./assets/Cancel.png")}
+      />
+    </View>
+  );
+};
+
+export default Page;
