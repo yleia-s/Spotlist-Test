e2cae3ff8df3c1ccef03240ee6a2e1cc374721f6
yleia-s <67092350+yleia-s@users.noreply.github.com>
2020-10-07 13:34:57 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import React from "react";
 import { View, Image, StyleSheet } from "react-native";
 
+const images = {
+  options: {
+    barber: require("./assets/Barber.png"),
+    beautician: require("./assets/Beautician.png"),
+    makeup: require("./assets/Makeup.png"),
+    nails: require("./assets/Nails.png"),
+    esthetician: require("./assets/Esthetician.png"),
+    masseuse: require("./assets/Message.png"),
+  },
+  background: require("./assets/Background.png"),
+  logo: require("./assets/splash.png"),
+  selectIndustry: require("./assets/selectIndustry.png"),
+  cancel: require("./assets/Cancel.png"),
+};
+
 const styles = StyleSheet.create({
   container: {
     paddingTop: 50,
@@ -17,32 +32,21 @@ const styles = StyleSheet.create({
 const Page = () => {
   return (
     <View style={styles.container}>
-      <Image
-        style={{ width: 150, height: 150 }}
-        source={require("./assets/splash.png")}
-      />
-      <Image
-        style={{ height: 50 }}
-        source={require("./assets/selectIndustry.png")}
-      />
-      <Image style={styles.stretch} source={require("./assets/Barber.png")} />
-      <Image
-        style={styles.stretch}
-        source={require("./assets/Beautician.png")}
-      />
-      <Image style={styles.stretch} source={require("./assets/Makeup.png")} />
-      <Image style={styles.stretch} source={require("./assets/Nails.png")} />
-      <Image
-        style={styles.stretch}
-        source={require("./assets/Esthetician.png")}
-      />
-      <Image style={styles.stretch} source={require("./assets/Message.png")} />
+      <Image style={{ width: 150, height: 150 }} source={images.logo} />
+      <Image style={{ height: 50 }} source={images.options.selectIndustry} />
+      <Image style={styles.stretch} source={images.options.barber} />
+      <Image style={styles.stretch} source={images.options.beautician} />
+      <Image style={styles.stretch} source={images.options.makeup} />
+      <Image style={styles.stretch} source={images.options.nails} />
+      <Image style={styles.stretch} source={images.options.esthetician} />
+      <Image style={styles.stretch} source={images.options.masseuse} />
       <Image
         style={{ flexDirection: "column", justifyContent: "center" }}
-        source={require("./assets/Cancel.png")}
+        source={images.cancel}
       />
     </View>
   );
 };
 
 export default Page;
+
