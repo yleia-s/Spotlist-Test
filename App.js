9e9874ff36eef1436db35297a7a599d8afd27255
yleia-s <67092350+yleia-s@users.noreply.github.com>
2020-10-07 14:48:46 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,31 @@
 import React from "react";
 import { View, Image, StyleSheet } from "react-native";
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1d7264",
+    paddingTop: 75,
+  },
+  logo: {
+    paddingTop: 125,
+    padding: 75,
+    width: 75,
+    height: 75,
+    alignSelf: "center",
+  },
+  selectIndustry: {
+    alignSelf: "center",
+  },
+  options: {
+    alignSelf: "center",
+    width: 400,
+  },
+  cancel: {
+    alignSelf: "center",
+  },
+});
+
 const images = {
   options: {
     barber: require("./assets/Barber.png"),
@@ -16,37 +41,20 @@ const images = {
   cancel: require("./assets/Cancel.png"),
 };
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-  },
-  stretch: {
-    width: 500,
-    height: 50,
-    resizeMode: "stretch",
-    flexDirection: "column",
-    justifyContent: "space-evenly",
-  },
-});
-
 const Page = () => {
   return (
     <View style={styles.container}>
-      <Image style={{ width: 150, height: 150 }} source={images.logo} />
-      <Image style={{ height: 50 }} source={images.options.selectIndustry} />
-      <Image style={styles.stretch} source={images.options.barber} />
-      <Image style={styles.stretch} source={images.options.beautician} />
-      <Image style={styles.stretch} source={images.options.makeup} />
-      <Image style={styles.stretch} source={images.options.nails} />
-      <Image style={styles.stretch} source={images.options.esthetician} />
-      <Image style={styles.stretch} source={images.options.masseuse} />
-      <Image
-        style={{ flexDirection: "column", justifyContent: "center" }}
-        source={images.cancel}
-      />
+      <Image style={styles.logo} source={images.logo} />
+      <Image style={styles.selectIndustry} source={images.selectIndustry} />
+      <Image style={styles.options} source={images.options.barber} />
+      <Image style={styles.options} source={images.options.beautician} />
+      <Image style={styles.options} source={images.options.makeup} />
+      <Image style={styles.options} source={images.options.nails} />
+      <Image style={styles.options} source={images.options.esthetician} />
+      <Image style={styles.options} source={images.options.masseuse} />
+      <Image style={styles.cancel} source={images.cancel} />
     </View>
   );
 };
 
 export default Page;
-
